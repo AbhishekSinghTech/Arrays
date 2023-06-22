6283231aee95f7c3aca072cf2f9c4b36ae7883e3
Abhishek Singh <136239184+AbhishekSinghTech@users.noreply.github.com>
2023-06-22 18:30:31 +0530

    Add files via upload
    Operation on Array

---
+++ Search.java
@@ -0,0 +1,19 @@
+
+public class Search {
+
+    static int search(int arr[],int n,int x){
+        for(int i=0;i<n;i++){
+            if(arr[i]==x){
+                return i;
+            }
+        }
+        return -1;
+    
+    }
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40};
+        int x=5;
+        System.out.println(search(arr, arr.length, x));
+    }
+
+}
