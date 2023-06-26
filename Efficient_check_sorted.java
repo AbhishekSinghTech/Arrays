3eb512fc76e2e8f3cb0ff1d566604fabde868fac
Abhishek Singh <136239184+AbhishekSinghTech@users.noreply.github.com>
2023-06-26 21:27:49 +0530

    Update Efficient_check_sorted.java

--- Efficient_check_sorted.java
+++ Efficient_check_sorted.java
@@ -1,3 +1,15 @@
 public class Efficient_check_sorted {
-    
+    boolean insorted(int[]arr){
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<arr[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[]arr={5,10,12,13};
+        Efficient_check_sorted ecs=new Efficient_check_sorted();
+        System.out.println(ecs.insorted(arr));
+    }
 }
